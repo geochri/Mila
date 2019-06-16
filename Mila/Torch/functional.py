e736004a55967bdf7f097281b4a0d2d50738ac27
Diganta Misra <mishradiganta91@gmail.com>
2019-06-16 11:25:52 +0530

    Added PyTorch Implementation for Mila

---
+++ Mila/Torch/functional.py
@@ -0,0 +1,17 @@
+'''
+Script provides functional interface for Mila activation function.
+'''
+
+# import pytorch
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+def mila(input, beta=1.0):
+    '''
+    Applies the Mila function element-wise:
+    Mila(x) = x * tanh(softplus(1 + β)) = x * tanh(ln(1 + exp(x+β)))
+
+    See additional documentation for mila class.
+    '''
+    return input * torch.tanh(F.softplus(input+beta))
---
+++ Mila/Torch/mila.py
@@ -0,0 +1,40 @@
+'''
+Applies the Mila function element-wise:
+Mila(x) = x * tanh(softplus(1 + β)) = x * tanh(ln(1 + exp(x+β)))
+'''
+
+# import pytorch
+import torch
+from torch import nn
+
+# import activation functions
+import Mila.Torch.functional as Func
+
+class mila(nn.Module):
+    '''
+    Applies the Mila function element-wise:
+    Mila(x) = x * tanh(softplus(1 + β)) = x * tanh(ln(1 + exp(x+β)))
+
+    Shape:
+        - Input: (N, *) where * means, any number of additional
+          dimensions
+        - Output: (N, *), same shape as the input
+
+    Examples:
+        >>> m = mila(beta=1.0)
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+
+    '''
+    def __init__(self, beta=1.0):
+        '''
+        Init method.
+        '''
+        super().__init__()
+        self.beta = beta
+
+    def forward(self, input):
+        '''
+        Forward pass of the function.
+        '''
+        return Func.mila(input, self.beta)
